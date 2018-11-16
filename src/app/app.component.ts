91e7bb88c5081f0c2fc43a894f881722cdfe092c
khayati iteb <khayati.iteb@gmail.com>
2018-11-16 11:03:45 +0100

    two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isAuth = false;
   title = 'mon-projet-angular';
+  
+  constructor() {
+  }
+  onAllumer() {
+    console.log('On allume tout !');
+}
 }
